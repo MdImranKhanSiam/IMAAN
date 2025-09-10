4235ca6796e8b9084bc66e5f785a7e5ddbf03350
Imran Khan <mdimrankhansiam@gmail.com>
2025-09-10 18:55:31 +0600

    Version 8

--- imaan/lib/all_pages/pages/qibla_folder/qibla_screen.dart
+++ imaan/lib/all_pages/pages/qibla_folder/qibla_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async'; // Make sure to import this
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_qiblah/flutter_qiblah.dart';
@@ -13,21 +14,42 @@ Animation<double>? animation;
 AnimationController? _animationController;
 double begin = 0.0;
 
-class _QiblaScreenState extends State<QiblaScreen> with SingleTickerProviderStateMixin {
+class _QiblaScreenState extends State<QiblaScreen>
+    with SingleTickerProviderStateMixin {
+  late StreamSubscription _qiblahSubscription;
 
   @override
   void initState() {
-    _animationController = AnimationController(vsync: this, duration: const Duration(microseconds: 500));
-    animation = Tween(begin: 0.0, end: 0.0).animate(_animationController!);
     super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    animation = Tween(begin: 0.0, end: 0.0).animate(_animationController!);
+
+    // Subscribe to the qiblah stream
+    _qiblahSubscription = FlutterQiblah.qiblahStream.listen((qiblahDirection) {
+      // Update the animation based on new qiblah direction
+      animation = Tween(
+        begin: begin,
+        end: (qiblahDirection.qiblah * (pi / 180) * -1),
+      ).animate(_animationController!);
+      begin = (qiblahDirection.qiblah * (pi / 180) * -1);
+      _animationController!.forward(from: 0); // Animate the compass
+    });
+  }
+
+  @override
+  void dispose() {
+    // Cancel the subscription when the widget is disposed
+    _qiblahSubscription.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // backgroundColor: const Color.fromARGB(255, 48, 48, 48),
-        //backgroundColor: const Color.fromARGB(255, 240, 233, 233),
         body: StreamBuilder(
           stream: FlutterQiblah.qiblahStream,
           builder: (context, snapshot) {
@@ -41,21 +63,10 @@ class _QiblaScreenState extends State<QiblaScreen> with SingleTickerProviderStat
             }
 
             final qiblahDirection = snapshot.data;
-            animation = Tween(begin: begin, end: (qiblahDirection!.qiblah * (pi / 180) * -1)).animate(_animationController!);
-            begin = (qiblahDirection.qiblah * (pi / 180) * -1);
-            _animationController!.forward(from: 0);
-
             return Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // Text(
-                  //   "${qiblahDirection.direction.toInt()}°",       //Shows Degree Of Rotation
-                  //   style: const TextStyle(
-                  //     color: Colors.white,
-                  //     fontSize: 24,
-                  //   ),
-                  // ),
                   const SizedBox(height: 10),
                   SizedBox(
                     height: 300,
